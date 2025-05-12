5e08a80bd9025f927cdd87aefd69689c3e1277a5
VPA2103 <150929603+VPA2103@users.noreply.github.com>
2025-05-12 15:36:18 +0700

    xong bai 2

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 use Illuminate\Support\Facades\Auth;
+
+
+use App\Http\Controllers\HomeController;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->name('home.index');
