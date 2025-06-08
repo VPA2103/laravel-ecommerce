8d8c3b9eb2e8656bb33345adb601911ae55b8cf9
VPA2103 <150929603+VPA2103@users.noreply.github.com>
2025-06-08 14:32:54 +0700

    success lesson 23: product sorting

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -7,10 +7,36 @@ use App\Models\Product;
 
 class ShopController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::orderBy('created_at','DESC')->paginate(12);
-        return view('shop', compact('products'));
+        $size = $request->query('size') ? $request->query('size') : 12;
+        $o_column = "";
+        $o_order = "";
+        $order = $request->query('order') ? $request->query('order') : -1;
+        switch ($order){
+            case 1: 
+                $o_column = 'created_at';
+                $o_order='DESC';
+                break;
+            case 2:
+                $o_column = 'created_at';
+                $o_order = 'ASC';
+                break;
+            case 3:
+                $o_column = 'sale_price';
+                $o_order = 'ASC';
+                break;
+            case 4:
+                $o_column = 'sale_price';
+                $o_order = 'DESC';
+                break;
+            default:
+            $o_column = 'id';
+            $o_order = 'DESC';
+
+        }
+        $products = Product::orderBy($o_column,$o_order)->paginate($size);
+        return view('shop', compact('products','size','order'));
     }
 
     public function product_details($product_slug){
@@ -18,4 +44,5 @@ class ShopController extends Controller
         $products = Product::where('slug','<>',$product_slug)->get()->take(8);
         return view('details',compact('product','products'));
     }
+
 }
