e4b7d10abf81de7a2ee84d27f2de2c19694b5c86
VPA2103 <150929603+VPA2103@users.noreply.github.com>
2025-06-15 02:36:38 +0700

    cc

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -17,6 +17,9 @@ class ShopController extends Controller
         $order = $request->query('order') ? $request->query('order') : -1;
         $f_brands = $request->query('brands');
         $f_categories = $request->query('categories');
+        $min_price = $request->query('min') ? $request->query('min') : 1;
+        $max_price = $request->query('max') ? $request->query('max') : 500;
+
         switch ($order) {
             case 1:
                 $o_column = 'created_at';
@@ -46,7 +49,7 @@ class ShopController extends Controller
             $query->whereIn('category_id', explode(',', $f_categories))->orWhereRaw("'" . $f_categories . "'=''");
         })
             ->orderBy($o_column, $o_order)->paginate($size);
-        return view('shop', compact('products', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories'));
+        return view('shop', compact('products', 'size', 'order', 'brands', 'f_brands', 'categories', 'f_categories', 'min_price', 'max_price'));
     }
 
     public function product_details($product_slug)
