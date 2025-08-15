0ecfceb9bda26f9476eeb74d937ca76261fa6096
VPA2103 <150929603+VPA2103@users.noreply.github.com>
2025-08-15 23:29:19 +0700

    feat: xong thanh toan san pham

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Trasaction extends Model
+class Transaction extends Model
 {
     use HasFactory;
     
