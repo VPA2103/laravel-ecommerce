20f3f9dafa62dcc944318280a616205ac36d3350
thangvu12321 <vu71394@gmail.com>
2025-08-19 04:03:13 +0700

    Da Lam Xong Bai 44

--- resources/views/user/order-details.blade.php
+++ resources/views/user/order-details.blade.php
@@ -120,6 +120,9 @@
                 </div>
             </div>
             <div class="table-responsive">
+                    @if (Session::has('status'))
+                        <p class="alert alert-success">{{ Session::get('status') }}</p>
+                    @endif
                 <table class="table table-bordered">
                     <tr>
                         <th>Order No</th>
@@ -265,8 +268,39 @@
                 </tbody>
             </table>
         </div>
+                @if($order->status=="ordered")
+                <div class="wg-box mt-5 text-right">
+                    <form action="{{route('user.order.cancel')}}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <input type="hidden" name="order_id" value="{{$order->id}}" />
+                        <button type="button" class="btn btn-danger cancel-order">Cancel Order</button>
+                    </form>
+                </div>
+                @endif
             </div>            
         </div>
     </section>
 </main>
 @endsection
+@push('scripts')
+<script>
+    $(function(){
+        $('.cancel-order').on('click',function(e){
+            e.preventDefault();
+            var form= $(this).closest('form');
+            swal({
+                title: 'Are you sure?',
+                text: "You want to cancel this order?",
+                type: 'warning',
+                buttons: ["Cancel","Yes"],
+                confirmButtonColor: '#dc3545',
+            }).then(function(result){
+                if(result){
+                    form.submit();
+                }
+            })
+        })
+    })
+</script>
+@endpush
